299340144d42a334185e14f736e04434395f9c07
Crif14 <csqueo22@gmail.com>
2024-10-08 09:51:26 +0200

    Css Added

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,55 @@
-//Qui scriviamo il codice JS
+
 /*
-Scrivere una funzione che generi un numero casuale intero compreso fra un minimo e un max
+    Come accedere ad un elemento in Javascript
+    Accedo ad un elemento in JS tramite il metodo querySelector
+    che fa parte dell'oggetto document
+    --> document.querySelector()
+    querySelector è un metodo (funzione) dell'oggetto document
+    a cui voglio accedere come stringa
+    -restetuisce l'elemento del DOM "matchato" dal selettore
+    --> possiamo asssegnare una variabile
+    --> querySelector mi restituisce un oggetto di tipo Element
+    (un elemento HTML); nel caso degli elementi <input>
+    posso accedere agli attributi del campo utilizzando il punto
+    es. -->
+    let userField = document.querySelector("#guess")
+    console.log(userField.value) --> logga in console il cont
 
-function generateRandomInteger(min, max){
-    // qui devo usare Math.random
-}
+Per gestire l'evento che poi andrà ad avviare il controlli sui numeri
+inseriti dell'utente ha bisogno di 3 cose:
+    1 - l'elemento che scatenerà l'evento (il button nel nostro caso)
+    2 - il nome dell'evento
+    3 - le cose da fare dopo che l'evento è stato scatenato
 
-Usare questa funzione per sviluppare il gioco "indovina il numero":
-generare un numero casuale compreso fra 1 e 100 
-chiedere in input all'utente un numero finchè non esaurisce 5 tentativi
-fare sanitize dell'input
-se il numero inserito dall'utente è maggiore del numero generato stampa in console la stringa "Troppo grande"
-se il numero inserito dall'utente è minore del numero generato stampa in console la stringa "Troppo piccolo"
-se il numero inserito dall'utente è uguale del numero generato stampa in console la stringa "Bravo, hai vinto"
-Il gioco finisce se l'utente esaurisce i 5 tentativi o se indovina
+    1 --> è il button --> uso querySelector per accedere all'elemento
+    2 --> l'evento che scatenerà il flusso di gioco è il click
+    3 --> sanitize if/else per controllo numero e console.log
+
+    per gestire un evento su un elemento HTML posso usare una funzione
+    che si chiama addEventListener
+
+        let btn = document.querySelector("#guessBtn") --> elemento al punto 1
+        function handleClick(event){
+        console.log(event)
+        } --> cose da fare (punto 3)
+        btn.addEventListener("click" handleClick)
+
+addEventListener è un metodo (funzione) dell'elemento HTML al punto 1 
+    parametri:
+        - il nome dell'evento come stringa
+        - la funzione da eseguire al verificarsi dell'evento (DEVE avere come oggetto argomento EVENT)
+
+nella funzione handleClick avrò tutti i controlli del gioco:
+    - prendo il valore inserito dall'utente
+    -sanitize dell'input
+    - parseInt dell'input
+    if/else con console.log
 */
 
 
-function generateRandomInteger(min=1, max=100){
-    num = Math.floor(Math.random() * (max - min) + min);
+
+function generateRandomInteger(min=0, max=100){
+    let num = Math.floor(Math.random() * (max - min) + min);
     return num
 }
 
@@ -32,38 +63,50 @@ function sanitize(Var){
     return sanificata
 }
 
+function handleClick(event) {
+    let input = document.querySelector("#guess").value;
+    numeroInserito = sanitize(input);
+
+    if (isNaN(numeroInserito)) {
+        console.log("Inserisci un numero valido");
+    } else {
+        if (numeroInserito > numEFFETIVO) {
+            console.log("Il numero '" + numeroInserito + "' è troppo grande");
+            tentativi -= 1;
+            contatore += 1;
+            console.log("Hai ancora " + tentativi + " tentativi");
+        } else if (numeroInserito < numEFFETIVO) {
+            console.log("Il numero '" + numeroInserito + "' è troppo piccolo");
+            tentativi -= 1;
+            contatore += 1;
+            console.log("Hai ancora " + tentativi + " tentativi");
+        } else if (numeroInserito === numEFFETIVO) {
+            console.log("Bravo, hai vinto!");
+            indovinato = true;
+            document.querySelector("#guess").disabled = true;
+            document.querySelector("#guessBtn").disabled = true;
+        }
+    }
+    if (tentativi === 0 && !indovinato) {
+        console.log("Mi dispiace, hai perso. Il numero era " + numEFFETIVO);
+        document.querySelector("#guess").disabled = true;
+        document.querySelector("#guessBtn").disabled = true;
+    }
+
+}
+
+
+
 //PROGRAMMA PRINCIPALE
+console.log("Benvenuto a 'INDOVINA IL NUMERO!'");
+let numeroInserito
 let tentativi = 5
 let contatore = 0
 let indovinato = false
 let numEFFETIVO = generateRandomInteger()
-let numeroInserito 
-console.log("Benvenuto a 'INDOVINA IL NUMERO!'")
-while (contatore<5 && indovinato===false){
-    console.log("Hai ancora " + tentativi + " tentativi")
-    do{
-        let input = prompt("Inserisci un numero")
-        numeroInserito = sanitize(input)
-        if (isNaN(numeroInserito)) {
-            console.log("Inserisci un numero valido")
-        } else {
-            break;
-        }
-    }while(true)
+let userField = document.querySelector("#guess")
+let btn = document.querySelector("#guessBtn")
+    btn.addEventListener("click", handleClick)
 
-    if(numeroInserito>numEFFETIVO){
-        console.log("Il numero '" + numeroInserito + "' è troppo grande")
-        tentativi-=1
-        contatore+=1
-    }else if(numeroInserito<numEFFETIVO){
-        console.log("Il numero '" + numeroInserito + "' è troppo piccolo")
-        tentativi-=1
-        contatore+=1
-    }else if(numeroInserito===numEFFETIVO){
-        console.log("Bravo, hai vinto!")
-        indovinato=true
-    }
-}
-if (!indovinato) {
-    console.log("Mi dispiace, hai perso. Il numero era " + numEFFETIVO);
-}
+
+
